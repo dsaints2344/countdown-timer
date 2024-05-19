a2ab2d7ccc29c30050c7fe5a7143ff9dc2356166
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2024-05-18 20:03:46 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+
+const colors = {
+  primary: {
+    grayishBlue: "hsl(237, 18%, 59%)",
+    softRed: "hsl(345, 95%, 68%)",
+  },
+  neutral: {
+    white: "hsl(0, 0%, 100%)",
+    darkDesaturatedBlue: "hsl(236, 21%, 26%)",
+    veryDarkBlue: "hsl(235, 16%, 14%)",
+    veryDarkMostlyBlackBlue: "hsl(234, 17%, 12%)",
+  },
+};
+
+const customTheme = extendTheme({ colors })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
-)
+);
