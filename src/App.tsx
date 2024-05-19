b86ebea357a2d19024379747245ccc352297e53b
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2024-05-18 21:09:46 -0400

    Creating layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Layout from "./components/Layout"
 
 const App = () => {
-
   return (
     <>
+      <Layout />
     </>
   )
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,37 @@
+import { Box } from "@chakra-ui/react";
+import { FC, ReactNode } from "react";
+import bgStars from "../assets/bg-stars.svg";
+import patternHills from "../assets/pattern-hills.svg"
+
+interface Props {
+    children?: ReactNode;
+
+}
+
+const Layout: FC<Props> = ({ children }) => {
+    return <>
+        <Box backgroundImage={bgStars}
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            height="100vh"
+            minWidth="100vw"
+            minHeight="100vh"
+            backgroundColor="neutral.veryDarkMostlyBlackBlue"
+        >
+            {children}
+            <Box
+                position="absolute" // Position the footer at the bottom
+                bottom="0" // Set the bottom position to 0
+                left="0" // Set the left position to 0 (optional, for full width)
+                right="0" // Set the right position to 0 (optional, for full width)
+                width="100%" // Set the width to 100% (optional, adjust as needed)
+                height="193px" // Set the height of the footer (adjust as needed)
+                backgroundImage={patternHills}
+                backgroundPosition="center" // Optional: control footer image position
+                backgroundRepeat="no-repeat" // Optional: control footer image repeat
+            />
+        </Box>
+    </>;
+}
+
+export default Layout
