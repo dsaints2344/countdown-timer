0a54baa8cf6edcc86c585554410ade860e5194cf
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2024-06-08 22:30:43 -0400

    Update Timer.tsx

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -7,7 +7,21 @@ import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 const Timer = () => {
 
     return <Box alignItems="center">
-        <FlipClockCountdown to={new Date().getTime() + 24 * 3600 * 1000 + 5000} ></FlipClockCountdown>
+        <FlipClockCountdown
+            digitBlockStyle={{
+                color: '#fb6087',
+                fontWeight: '700',
+                width: "80px",
+                fontSize: "67px",
+                fontFamily: "'Red Hat Text', sans-serif",
+                backgroundColor: "#343650"
+            }}
+            labelStyle={{
+                color: "#6C6A83",
+                fontFamily: "'Red Hat Text', sans-serif",
+                fontSize: "14px"
+            }}
+            to={new Date().getTime() + 24 * 3600 * 1000 + 5000} showSeparators={false} spacing={{ clock: 10 }}></FlipClockCountdown>
     </Box>
 }
 
